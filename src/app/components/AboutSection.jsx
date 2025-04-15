a4f557846590e204228d0f44ca07b9f252fccd5d
adamyatiwari12 <adamyatiwari99@gmail.com>
2025-04-16 01:33:31 +0530

    initial_commit

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,94 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React.js</li>
+        <li>Next.js</li>
+        <li>Python</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Fullstack Academy of Code</li>
+        <li>University of California, Santa Cruz</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>AWS Cloud Practitioner</li>
+        <li>Google Professional Cloud Developer</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} alt="Couldn't fetch image" />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+          I’m a frontend developer dedicated to crafting clean, responsive, and user-friendly web applications. I work primarily with HTML, CSS, JavaScript, and React, and enjoy turning ideas into interactive, accessible interfaces. Beyond web development, I have a strong interest in algorithmic problem-solving — I’ve solved over 100 challenges on LeetCode and achieved a Codeforces rating of 1000, which has helped me strengthen my logical thinking and coding efficiency.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+/** @type {import('tailwindcss').Config} */
+const {
+  gray,
+  red,
+  yellow,
+  green,
+  blue,
+  indigo,
+  purple,
+  pink,
+  sky,
+  slate,
+  stone,
+  neutral,
+  emerald,
+  // ...include other needed colors
+} = require("tailwindcss/colors");
+
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+    colors: {
+      gray,
+      red,
+      yellow,
+      green,
+      blue,
+      indigo,
+      purple,
+      pink,
+      sky,
+      slate,
+      stone,
+      neutral,
+      emerald,
+      primary: purple,
+      secondary: pink,
+    },
+  },
+  plugins: [],
+};
