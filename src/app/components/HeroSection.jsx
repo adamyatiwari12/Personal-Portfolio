eeb89ae52ca1b22e8f6a63c642ed398eb7d5fb9b
adamyatiwari12 <adamyatiwari99@gmail.com>
2025-04-16 23:54:34 +0530

    fix image issues

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -60,8 +60,8 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/Personal Image.JPG"
-              alt="hero image"
+              src="/images/personal-image.jpg"
+              alt="Couldn't fetch image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full"
               width={400}
               height={400}
