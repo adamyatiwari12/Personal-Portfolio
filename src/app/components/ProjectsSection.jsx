39c13268d63a18d899c586a23ef2ed475cd07fd6
adamyatiwari12 <adamyatiwari99@gmail.com>
2025-05-31 00:55:45 +0530

    add more projects

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -5,17 +5,36 @@ import ProjectTag from "./ProjectTag";
 import { motion, useInView } from "framer-motion";
 
 const projectsData = [
+
   {
     id: 1,
+    title: "Blink-Tic-Tac-Toe",
+    description: "A playful 2-player emoji-based Tic Tac Toe with a twist—older moves vanish, adding strategic depth. Align 3 emojis to win, but plan carefully as only your latest 3 remain on the board!",
+    image: "/images/projects/blink-tic-tac.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/adamyatiwari12/Blink-Tic-Tac-Toe",
+    previewUrl: "https://blink-tic-tac-toe-one.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "UX Audit Tool",
+    description: "An intelligent UX audit tool that analyzes websites or HTML to give actionable improvement tips. It checks structure, content, and design, with AI-powered feedback on clarity, hierarchy, and trust.",
+    image: "/images/projects/ux-audit.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/adamyatiwari12/UX-Audit-Tool",
+    previewUrl: "https://ux-audit-tool.vercel.app/",
+  },
+  {
+    id: 3,
     title: "NutriTrack",
-    description: "A responsive web app to help users search for food items, log meals, and monitor their daily nutrition intake.",
+    description: "A smart nutrition tracker that lets users log meals, track calories and macros, and visualize daily progress. With real-time food search and interactive charts, it promotes healthy eating through clear insights.",
     image: "/images/projects/nutri-track.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/adamyatiwari12/Nutrition-Tracker",
     previewUrl: "https://nutrition-tracker-mu.vercel.app/",
   },
   {
-    id: 2,
+    id: 4,
     title: "Job Huntly Demo Website",
     description: "A responsive job hunting demo app where users can browse jobs, apply easily, and track applications.",
     image: "/images/projects/job-huntly.JPG",
@@ -24,7 +43,7 @@ const projectsData = [
     previewUrl: "https://job-huntly-demo-website.vercel.app/",
   },
   {
-    id: 3,
+    id: 5,
     title: "Expense Tracker and Splitter",
     description: "A responsive web app to track and split expenses in real-time with a clean and intuitive UI.",
     image: "/images/projects/expense.JPG",
@@ -32,15 +51,15 @@ const projectsData = [
     gitUrl: "https://github.com/adamyatiwari12/Expense-Tracker-and-Splitter",
     previewUrl: "https://expense-tracker-and-splitter.vercel.app/",
   },
-  // {
-  //   id: 4,
-  //   title: "Tic Tac Toe Game",
-  //   description: "A classic Tic Tac Toe game, allowing two players to compete in a turn-based match.",
-  //   image: "/images/projects/tic-tac.JPG",
-  //   tag: ["All", "Web"],
-  //   gitUrl: "https://github.com/adamyatiwari12/Tic-Tac-Toe",
-  //   previewUrl: "https://adamyatiwari12.github.io/Tic-Tac-Toe/",
-  // }
+  {
+    id: 6,
+    title: "Tic Tac Toe Game",
+    description: "A classic Tic Tac Toe game, allowing two players to compete in a turn-based match.",
+    image: "/images/projects/tic-tac.JPG",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/adamyatiwari12/Tic-Tac-Toe",
+    previewUrl: "https://adamyatiwari12.github.io/Tic-Tac-Toe/",
+  }
 ];
 
 const ProjectsSection = () => {
